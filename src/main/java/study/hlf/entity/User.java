da5d689d17ac9bf5b508d7b88cfde8b0f1ee6148
qkrtjdrbs <notlce@naver.com>
2021-12-15 19:06:08 +0900

    홈 화면 설정

--- src/main/java/study/hlf/entity/User.java
+++ src/main/java/study/hlf/entity/User.java
@@ -1,20 +1,24 @@
 package study.hlf.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id @GeneratedValue
     @Column(name = "user_id")
     private Long id;
 
-    private String email;
     private String username;
     private String password;
+    private String email;
 
     @Enumerated(value = EnumType.STRING)
     private Role role;
