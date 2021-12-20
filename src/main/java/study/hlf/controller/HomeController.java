da34777153bc20c438ef3b1f36c9fe56b9685e5e
qkrtjdrbs <notlce@naver.com>
2021-12-20 23:30:13 +0900

    로그인 했을 때 네비게이션 바 구분

--- src/main/java/study/hlf/controller/HomeController.java
+++ src/main/java/study/hlf/controller/HomeController.java
@@ -1,13 +1,24 @@
 package study.hlf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import study.hlf.Const;
+import study.hlf.entity.User;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
-        return "home";
+    public String home(
+            @SessionAttribute(required = false, name = Const.LOGIN_USER) User loginUser,
+            Model model
+    ){
+        if(loginUser == null){
+            return "home";
+        }
+        model.addAttribute("user", loginUser);
+        return "loginHome";
     }
 }
