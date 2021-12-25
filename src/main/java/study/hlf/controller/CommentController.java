807fe4f10428c9de3fe3b91356a1c013e528cd15
qkrtjdrbs <notlce@naver.com>
2021-12-25 23:19:31 +0900

    댓글 작성 에러 처리

--- src/main/java/study/hlf/controller/CommentController.java
+++ src/main/java/study/hlf/controller/CommentController.java
@@ -20,7 +20,10 @@ public class CommentController {
     @PostMapping("/write")
     public String save(@RequestBody CommentFormDto form,
                        HttpServletRequest request){
-        commentService.writeComment(form);
+        Long id = commentService.writeComment(form);
+        if(id == null){
+            throw new RuntimeException();
+        }
         String referer = request.getHeader("referer");
         return "redirect:" + referer;
     }
