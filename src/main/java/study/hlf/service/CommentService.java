c5301bd9c42a1cac7c0be21931e6a919462d1f5b
qkrtjdrbs <notlce@naver.com>
2021-12-25 23:14:30 +0900

    간단한 댓글 검증 위해 ajax 댓글 작성 구현

--- src/main/java/study/hlf/service/CommentService.java
+++ src/main/java/study/hlf/service/CommentService.java
@@ -2,6 +2,7 @@ package study.hlf.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import study.hlf.dto.CommentFormDto;
 import study.hlf.entity.Board;
 import study.hlf.entity.Comment;
@@ -14,12 +15,14 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CommentService {
 
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
     private final CommentRepository commentRepository;
 
+    @Transactional
     public Long writeComment(CommentFormDto form){
         Optional<User> user = userRepository.findById(form.getUser_id());
         Optional<Board> board = boardRepository.findById(form.getBoard_id());
