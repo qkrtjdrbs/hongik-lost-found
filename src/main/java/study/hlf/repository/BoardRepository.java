15d764aa26ccfa8540f92218b76cdeeafc79b0d3
qkrtjdrbs <notlce@naver.com>
2021-12-27 15:36:07 +0900

    게시물 댓글 불러올 때 N+1 문제 해결

--- src/main/java/study/hlf/repository/BoardRepository.java
+++ src/main/java/study/hlf/repository/BoardRepository.java
@@ -1,7 +1,15 @@
 package study.hlf.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.hlf.entity.Board;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
+
+    @Override
+    @Query("select b from Board b left join fetch b.comments c left join fetch c.user where b.id = :id")
+    Optional<Board> findById(@Param("id") Long id);
 }
