992b8f693170900cfe27003fec2be8e5377b6bf2
qkrtjdrbs <notlce@naver.com>
2021-12-30 19:42:15 +0900

    comment를 fragment로 따로 분리

--- src/main/java/study/hlf/controller/CommentController.java
+++ src/main/java/study/hlf/controller/CommentController.java
@@ -4,30 +4,32 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import study.hlf.dto.CommentDto;
+import study.hlf.Const;
 import study.hlf.dto.CommentFormDto;
 import study.hlf.entity.Comment;
+import study.hlf.entity.User;
 import study.hlf.service.CommentService;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/comment")
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/write")
+    @PostMapping("/comment/write")
     public String save(@RequestBody CommentFormDto form,
-                       Model model){
+                       Model model,
+                       @SessionAttribute(name = Const.LOGIN_USER, required = false) User loginUser){
         Comment comment = commentService.writeComment(form);
         if(comment == null){
             throw new RuntimeException();
         }
 
         model.addAttribute("comment", comment);
+        if(loginUser != null){
+            model.addAttribute("user", loginUser);
+        }
 
         return "fragments/comment";
     }
