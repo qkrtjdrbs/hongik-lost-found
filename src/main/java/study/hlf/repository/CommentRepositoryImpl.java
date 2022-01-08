7015d6e2a007cdb71aaa49320a43c281f4ac35fe
qkrtjdrbs <notlce@naver.com>
2022-01-08 22:10:18 +0900

    무한x 대댓글 페이징 조회 구현

--- src/main/java/study/hlf/repository/CommentRepositoryImpl.java
+++ src/main/java/study/hlf/repository/CommentRepositoryImpl.java
@@ -1,22 +1,15 @@
 package study.hlf.repository;
 
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
 import study.hlf.entity.Comment;
-import study.hlf.entity.QBoard;
-import study.hlf.entity.QComment;
-import study.hlf.entity.QUser;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import static study.hlf.entity.QBoard.*;
 import static study.hlf.entity.QComment.*;
 import static study.hlf.entity.QUser.*;
 
@@ -37,25 +30,29 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom{
                 .orderBy(comment.parent.id.asc().nullsFirst(), comment.createdDate.asc())
                 .fetch();
 
-        JPAQuery<Comment> countQuery = queryFactory
-                .selectFrom(comment)
-                .innerJoin(comment.board, board)
-                .where(comment.board.id.eq(postId));
+        int total = content.size();
+        content = convertNestedStructure(content);
+        content = new ArrayList<>(content.subList(pageable.getPageNumber() / 10,
+                Math.min(pageable.getPageNumber() / 10 + 10, total)));
 
-        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetch().size());
+        return new PageImpl<>(content, pageable, total);
     }
 
-    /*private List<Comment> convertNestedStructure(List<Comment> comments) {
+    private List<Comment> convertNestedStructure(List<Comment> comments) {
         List<Comment> result = new ArrayList<>();
         for (int i = 0; i < comments.size(); i++) {
-            result.add(comments.get(i));
+            if(comments.get(i).getParent() == null){
+                result.add(comments.get(i));
+            }
             for (int j = i+1; j < comments.size(); j++) {
-                if(comments.get(j).getParent() == null) continue;
+                if(comments.get(j).getParent() == null){
+                    continue;
+                }
                 if(comments.get(j).getParent().getId() == comments.get(i).getId()){
                     result.add(comments.get(j));
                 }
             }
         }
         return result;
-    }*/
+    }
 }
